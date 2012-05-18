56c4781931f7c662132a26cdba73ac5d045b926a
marco.corvi@gmail.com <marco.corvi@gmail.com@9f98834b-2f69-cd57-b29d-fe4091e86c52>
2012-05-18 18:05:42 +0000

    three files i missed to add ...

---
+++ trunk/TopoDroid/src/com/android/DistoX/DistoXSurveyInfo.java
@@ -0,0 +1,22 @@
+/** @file DistoXSurveyInfo.java
+ *
+ * @author marco corvi
+ * @date may 2012
+ *
+ * @brief TopoDroid survey info (name, date, comment etc)
+ * --------------------------------------------------------
+ *  Copyright This sowftare is distributed under GPL-3.0 or later
+ *  See the file COPYING.
+ * --------------------------------------------------------
+ * CHANGES
+ */
+package com.android.DistoX;
+
+class DistoXSurveyInfo
+{
+  public long id;
+  public String name;
+  public String date;
+  public String comment;
+  public String team;
+}
